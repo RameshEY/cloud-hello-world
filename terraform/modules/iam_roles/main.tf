e1cd448020a4ab1592a97ef7c3b310c7f6e168cf
Marcus Barcelos <marcus.barcelos@bluesoft.com.br>
2017-05-30 16:04:29 -0300

    terraform configuration iam role

---
+++ terraform/modules/iam_roles/main.tf
@@ -0,0 +1,83 @@
+#http://docs.aws.amazon.com/AmazonECS/latest/developerguide/IAM_policies.html
+
+resource "aws_iam_role" "ecs_instance_role" {
+  name = "${var.app_name}_ecs_instance_role"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2008-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": ["ec2.amazonaws.com"]
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_instance_profile" "ecs" {
+  name = "${var.app_name}_ecs_instance_profile"
+  path = "/"
+  role = "${aws_iam_role.ecs_instance_role.name}"
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_ec2_role" {
+  role       = "${aws_iam_role.ecs_instance_role.id}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+####################
+resource "aws_iam_role" "ecs_service_role" {
+  name = "${var.app_name}_ecs_service_role"
+  path = "/ecs/"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2008-10-17",
+  "Statement": [
+    {
+      "Action": "sts:AssumeRole",
+      "Principal": {
+        "Service": ["ecs.amazonaws.com"]
+      },
+      "Effect": "Allow"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_service" {
+  role       = "${aws_iam_role.ecs_service_role.id}"
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceRole"
+}
+
+####################
+
+resource "aws_iam_role" "ecs_default_task_role" {
+  name = "${var.app_name}_ecs_default_task"
+  path = "/ecs/"
+
+  assume_role_policy = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Principal": {
+        "Service": ["ecs-tasks.amazonaws.com"]
+      },
+      "Action": "sts:AssumeRole"
+    }
+  ]
+}
+EOF
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_default_task" {
+  role       = "${aws_iam_role.ecs_default_task_role.id}"
+  policy_arn = "arn:aws:iam::aws:policy/AmazonS3ReadOnlyAccess"
+}
