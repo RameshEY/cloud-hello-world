042f549972d9fd31761ae42120aefc54396f9c48
Marcus Barcelos <mvlbarcelos@gmail.com>
2017-06-01 07:18:02 -0300

    build structure.

---
+++ build/build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+
+#/usr/bin/apache-maven-3.3.9/bin/mvn package
+mvn package
+tag=$(git describe --abbrev=0 --tags)
+docker build -f build/Dockerfile -t hello-world:$tag .
+
+$(aws ecr get-login --region eu-central-1)
+docker tag hello-world:$tag 156161676080.dkr.ecr.eu-central-1.amazonaws.com/hello-world:$tag
+docker tag hello-world:$tag 156161676080.dkr.ecr.eu-central-1.amazonaws.com/hello-world:latest
+
+docker push 156161676080.dkr.ecr.eu-central-1.amazonaws.com/hello-world:$tag
+docker push 156161676080.dkr.ecr.eu-central-1.amazonaws.com/hello-world:latest
+
+docker rmi hello-world:$tag
+docker rmi 156161676080.dkr.ecr.eu-central-1.amazonaws.com/hello-world:$tag
+docker rmi 156161676080.dkr.ecr.eu-central-1.amazonaws.com/hello-world::latest
