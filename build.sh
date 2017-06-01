4dd717f0628f925228034ef4579ed5c55a50d760
Marcus Barcelos <mvlbarcelos@gmail.com>
2017-05-31 21:40:46 -0300

    mvn package to build

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 $(aws ecr get-login --region eu-central-1)
+/usr/bin/apache-maven-3.3.9/bin/mvn package
 tag=$(git describe --abbrev=0 --tags)
 docker build -t hello-world:$tag .
 docker tag hello-world:$tag 156161676080.dkr.ecr.eu-central-1.amazonaws.com/hello-world:$tag
