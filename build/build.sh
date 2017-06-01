2d093fa63ed211a414c6388bef266014b5938d10
Marcus Barcelos <mvlbarcelos@gmail.com>
2017-06-01 20:34:28 -0300

    fix path.

--- build/build.sh
+++ build/build.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 tag=$(git describe --abbrev=0 --tags)
-sed -i '' 's/{RELEASE}/${tag}/g' ${WORKSPACE}/src/main/resources/application-*
+sed -i '' 's/{RELEASE}/${tag}/g' '${WORKSPACE}/src/main/resources/application-*'
 mvn package
 
-docker build -f ${WORKSPACE}build/Dockerfile -t hello-world:$tag .
+docker build -f build/Dockerfile -t hello-world:$tag .
 
 $(aws ecr get-login --region eu-central-1)
 docker tag hello-world:$tag 156161676080.dkr.ecr.eu-central-1.amazonaws.com/hello-world:$tag
