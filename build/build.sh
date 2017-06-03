b33505fbf12477fd4a5b76080b8c7b4697bdda3b
Marcus Barcelos <mvlbarcelos@gmail.com>
2017-06-03 02:20:52 -0300

    fix sed command to set release in properties

--- build/build.sh
+++ build/build.sh
@@ -2,8 +2,8 @@
 
 tag=$(git describe --abbrev=0 --tags)
 sed -i 's/{RELEASE}/'"$tag"'/g' ${WORKDIR}src/main/resources/application-development.properties
-sed -i 's/{RELEASE}/'"$tag"'/g' ${WORKDIR}src/main/resources/application-development.properties
-sed -i 's/{RELEASE}/'"$tag"'/g' ${WORKDIR}src/main/resources/application-development.properties
+sed -i 's/{RELEASE}/'"$tag"'/g' ${WORKDIR}src/main/resources/application-test.properties
+sed -i 's/{RELEASE}/'"$tag"'/g' ${WORKDIR}src/main/resources/application-production.properties
 mvn package
 
 docker build -f build/Dockerfile -t hello-world:$tag .
