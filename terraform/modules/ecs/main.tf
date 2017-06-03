44c9d2d1d127030c88958d6142980ae2e043c1fd
Marcus Barcelos <mvlbarcelos@gmail.com>
2017-06-02 22:33:11 -0300

    refactoring terraform configuration to create diferent enviroments.

--- terraform/modules/ecs/main.tf
+++ terraform/modules/ecs/main.tf
@@ -1,58 +1,7 @@
-module "ec2" {
-    source = "../ec2"
-    
-    app_name                = "${var.app_name}"
-    instance_type           = "${var.instance_type}"
-    ecs_aws_ami             = "${var.ecs_aws_ami}"
-    security_group_id       = "${module.network.sg_elb_to_app_id}"
-    iam_instance_profile    = "${module.iam_roles.ecs_instance_profile_arn}"
-    ecs_cluster_name        = "${aws_ecs_cluster.cluster_development.name}"
-    max_size                = "${var.max_size}"
-    min_size                = "${var.min_size}"
-    desired_capacity        = "${var.desired_capacity}"
-    private_subnet_ids      = "${module.network.private_subnet_ids}"
-}
-
-module "network" {
-    source = "../network"
-
-    app_name                = "${var.app_name}"
-    vpc_cidr                = "${var.vpc_cidr}"
-    public_subnet_cidrs     = "${var.public_subnet_cidrs}"
-    private_subnet_cidrs    = "${var.private_subnet_cidrs}"
-    availibility_zones      = "${var.availibility_zones}"
-}
-
-module "iam_roles" {
-    source = "../iam_roles"
-
-    app_name                = "${var.app_name}"
-}
-
-module "alb" {
-    source = "../alb"
-
-    route53_name            = "${var.route53_name}"
-    vpc_id                  = "${module.network.vpc_id}"
-    app_name                = "${var.app_name}"
-    security_group_id       = "${module.network.elb_https_http_id}"
-    public_subnet_ids       = "${module.network.public_subnet_ids}"
-    certificate_arn         = "${var.certificate_arn}"
-}
-
 resource "aws_ecs_cluster" "cluster_development" {
     name = "${var.app_name}_development"
 }
 
-resource "aws_ecs_cluster" "cluster_test" {
-    name = "${var.app_name}_test"
-}
-
-resource "aws_ecs_cluster" "cluster_production" {
-    name = "${var.app_name}_production"
-}
-
-# Simply specify the family to find the latest ACTIVE revision in that family.
 data "aws_ecs_task_definition" "hello" {
   task_definition = "${aws_ecs_task_definition.hello.family}"
 }
@@ -88,10 +37,10 @@ resource "aws_ecs_service" "development" {
   name          = "development"
   cluster       = "${aws_ecs_cluster.cluster_development.id}"
   desired_count = 2
-  iam_role      = "${module.iam_roles.ecs_service_role_arn}"
+  iam_role      = "${var.ecs_service_role_iam_role}"
 
   load_balancer {
-      target_group_arn  = "${aws_alb_target_group.development.arn}"
+      target_group_arn = "${var.alb_arn}"
       container_name    = "development"
       container_port    = 8080
   }
@@ -104,17 +53,17 @@ resource "aws_alb_target_group" "development" {
   name     = "tg-${var.app_name}-development"
   port     = 8080
   protocol = "HTTP"
-  vpc_id   = "${module.network.vpc_id}"
+  vpc_id   = "${var.vpc_id}"
   deregistration_delay = 60
 }
 
 resource "aws_alb_listener_rule" "host_based_routing" {
-  listener_arn = "${module.alb.alb_listener_arn}"
+  listener_arn = "${var.alb_listener_arn}"
     priority     = 99
   
     action {
       type             = "forward"
-      target_group_arn = "${aws_alb_target_group.development.arn}"
+      target_group_arn = "${var.alb_arn}"
     }
   
     condition {
--- terraform/modules/ecs/variables.tf
+++ terraform/modules/ecs/variables.tf
@@ -1,37 +1,10 @@
-variable "vpc_cidr" {
-    description = "VPC cidr block. Example: 172.16.0.0/16"
-}
-
 variable "app_name" {
     description = "The name of the application"
 }
+variable "vpc_id" {}
+variable "alb_arn" {}
+variable "alb_listener_arn" {}
+variable "ecs_service_role_iam_role" {}
 
-variable "destination_cidr_block" {
-    default     = "0.0.0.0/0"
-    description = "Specify all traffic to be routed either trough Internet Gateway or NAT to access the internet"
-}
-
-variable "private_subnet_cidrs" {
-    type        = "list"
-    description = "List of private cidrs: 172.16.21.0/24 and 172.16.22.0/24"
-}
-
-variable "public_subnet_cidrs" {
-    type        = "list"
-    description = "List of public cidrs: 172.16.0.0/24 and 172.16.1.0/24"
-}
-
-variable "availibility_zones" {
-    type        = "list"
-    description = "List of avalibility zones you want. eu-central-1a and eu-central-1b"
-}
-
-variable "instance_type" {}
-variable "ecs_aws_ami" {}
-variable "max_size" {}
-variable "min_size" {}
-variable "desired_capacity" {}
-variable "route53_name" {}
-variable "certificate_arn" {}
 
 
