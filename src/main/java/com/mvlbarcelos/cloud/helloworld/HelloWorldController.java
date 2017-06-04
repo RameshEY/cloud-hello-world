c1110bcca8390aa231700bbf45b402158ebc4835
Marcus Barcelos <mvlbarcelos@gmail.com>
2017-06-03 21:33:35 -0300

    configuring log to send log to elk

--- src/main/java/com/mvlbarcelos/cloud/helloworld/HelloWorldController.java
+++ src/main/java/com/mvlbarcelos/cloud/helloworld/HelloWorldController.java
@@ -1,17 +1,22 @@
 package com.mvlbarcelos.cloud.helloworld;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController(value = "/")
 public class HelloWorldController {
+
+	private static final Logger logger = LoggerFactory.getLogger(HelloWorldController.class);
 	
 	@Value("${message}")
 	private String message;
 
 	@GetMapping
 	public String hello(){
+		logger.info(message);
 		return message;
 	}
 
